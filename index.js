1840a21a7fcb8471f952dae7873eace49a876286
gasperpb <achilles@DESKTOP-PCATT53.localdomain>
2021-10-08 17:30:45 -0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const API_URL = 
+const API_URL = "https://jsonplaceholder.typicode.com"
+const xhr = new XMLHttpRequest();
+function onRequestHandler() {
+    if (this.readyState == 4 && this.status == 200) {
+        // 0 = UNSET, o método aberto não foi chamado. 
+        // 1 = OPENED, o método foi chamado.
+        // 2 = HEADERS_RECEIVED,  se está chamado o método send()
+        // 3 = LOADING,  está carregando, e recebendo a resposta 
+        // 4 = DONE, se há operação foi completada.
+
+        const data = JSON.parse(this.response)
+        const HTMLResponse = document.querySelector("#app");
+
+        const tpl = data.map((user) => `<li>${user.name}   ${user.email}</li>`);
+        HTMLResponse.innerHTML = `<ul>${tpl}</ul>`;
+
+
+    }
+}
+xhr.addEventListener("load", onRequestHandler)
+xhr.open("GET", `${API_URL}/users`);
+xhr.send();
